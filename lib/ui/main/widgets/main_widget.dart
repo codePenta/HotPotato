eb8ab55f95e801b1d30332a261b64c5113076554
PENTA <fhelm2001@gmail.com>
2025-12-15 11:42:50 +0100

    Removed unused imports in main_widget.dart

--- lib/ui/main/widgets/main_widget.dart
+++ lib/ui/main/widgets/main_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hot_potato/ui/timer/view_model/timer_viewmodel.dart';
 import 'package:hot_potato/ui/timer/widgets/timer_widget.dart';
-import 'package:provider/provider.dart';
 
 class MainWidget extends StatefulWidget {
   const MainWidget({super.key});
