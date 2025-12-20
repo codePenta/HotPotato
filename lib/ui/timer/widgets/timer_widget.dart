41c69a9ab1b9099b0bd4b28b21b764baf1884817
PENTA <fhelm2001@gmail.com>
2025-12-20 07:45:45 +0100

    Removed comment

--- lib/ui/timer/widgets/timer_widget.dart
+++ lib/ui/timer/widgets/timer_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hot_potato/ui/ending/widgets/game_ending_widget.dart';
 import 'package:hot_potato/ui/timer/view_model/timer_viewmodel.dart';
 import 'package:hot_potato/utils/animation_helper.dart';
-import 'package:provider/provider.dart'; // Import the Provider package
+import 'package:provider/provider.dart';
 import 'package:hot_potato/ui/math_challenge/widgets/math_challenge_widget.dart';
 
 class TimerWidget extends StatefulWidget {
